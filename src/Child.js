5b5ef14005c3647464f95dca07fbffbe1fcd0106
Alex Waller <alex@artofalexwaller.com>
2021-04-29 17:09:41 -0400

    commit tree f731a881b34052fa964dcb4e4c32585702947984

--- src/Child.js
+++ src/Child.js
@@ -3,13 +3,18 @@ import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
   render() {
+    console.log(this.props)
     return (
-      <div
+      // prop now being passed into a React event handler
+      <div onClick={() => this.props.handleColorChange(getRandomColor())}
+      // now that change changeColor of handleColorChange takes in an argument,
+      // we'll need to update the onClick of Child to be a function that invokes
+      // this.props.handleColorChange and passes in a random color as the argument
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
 }
-
+// sibling components cannot pass each other data
 export default Child
--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+/* 
+
+For information to propagate down the component tree, parents pass props
+their children
+
+For information to propagate up the component tree, we typically invoke callbacks
+that were passed from parents to children as props
+
+Components of the same level (sibling components) cannot communicat directly! We can 
+only communicate up and down the component tree. So if components need to share the
+same information, that state should live in the parent (or a more general ancestor)
+component.
+
+*/
